4cbcb61d5d6122e949710686b909f23c1553fab0
Kishan <kishanarustu@gmail.com>
2025-02-12 15:02:21 +0530

    Add project files.

---
+++ EcomCore/Services/DBConnection.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace EcomCore.Services
+{
+    public class DBConnection
+    {
+        private readonly string _connectionString;
+
+        public DBConnection(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Connection");
+        }
+
+        public int ExecuteNonQuery(string SpName, SqlParameter[] para)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(SpName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure; // 📌 Stored Procedure Mode
+                    if (para != null)
+                        cmd.Parameters.AddRange(para);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable ExecuteGetData(string SpName, SqlParameter[] para)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(SpName, conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure; // 📌 Set as Stored Procedure
+                    if (para != null)
+                        cmd.Parameters.AddRange(para);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                }
+            }
+            return dt;
+        }
+
+
+    }
+}
