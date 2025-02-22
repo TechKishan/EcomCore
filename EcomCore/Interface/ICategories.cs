8a28f35ce666a54c4fb4fffb5538be77cc15ca33
Kishan <kishanarustu@gmail.com>
2025-02-22 12:25:02 +0530

    Categories added

---
+++ EcomCore/Interface/ICategories.cs
@@ -0,0 +1,13 @@
+﻿using EcomCore.Models;
+using System.Data;
+using static EcomCore.Services.DBConnection;
+
+namespace EcomCore.Interface
+{
+    public interface ICategories
+    {
+        Task<DataTable> GetCategories();
+        Task<MessageFor> Insert(Categories data);
+        Task<MessageFor> Update(Categories data);
+    }
+}
